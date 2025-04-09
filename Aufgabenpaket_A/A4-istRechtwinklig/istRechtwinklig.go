6f31ebf652b8beb91cf16028f879d480cc587dc7
tomi <tomislav.varga@icloud.com>
2025-04-09 17:53:58 +0200

    initial commit

---
+++ Aufgabenpaket_A/A4-istRechtwinklig/istRechtwinklig.go
@@ -0,0 +1,8 @@
+package main
+
+// istRechtwinklig prüft, ob das durch die Seitenlängen x, y, z gebildete
+// Dreieck rechtwinklig ist.
+func istRechtwinklig(x, y, z int) bool {
+	// bitte korrigieren Sie den Rumpf dieser Funktion.
+	return true
+}
